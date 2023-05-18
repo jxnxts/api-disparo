074e4b34218fe7b01c8eb9eb555c3a98f2493255
Jonatas Soares <jonatasjc_@hotmail.com>
2023-05-18 00:01:55 -0300

    fix host rabbitmq

--- config/celery_config.py
+++ config/celery_config.py
@@ -11,7 +11,7 @@ def route_task(name, args, kwargs, options, task=None, **kw):
 
 
 class BaseConfig:
-    CELERY_BROKER_URL: str = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@rabbitmq:5672//")
+    CELERY_BROKER_URL: str = os.environ.get("CELERY_BROKER_URL", "amqp://guest:guest@localhost:5672//")
     CELERY_RESULT_BACKEND: str = os.environ.get("CELERY_RESULT_BACKEND", "rpc://")
 
     CELERY_TASK_QUEUES: list = (
